64a6d819c40b16d1bce57898019e1264273dfbd9
Random "Kritzsie" Hobo <kritzsie@gmail.com>
2022-04-04 19:19:37 +0100

    List of rendering states and current state are now separate

--- src/renderstates.cpp
+++ src/renderstates.cpp
@@ -4,26 +4,51 @@
 
 namespace kme {
 RenderFrame::RenderFrame(
-std::string texture, Rect<int> cliprect, Vec2f offset, float duration)
-: texture(texture), cliprect(cliprect), offset(offset), duration(duration) {}
+  std::string texture, Rect<int> cliprect, Vec2f offset, float duration
+) : texture(texture), cliprect(cliprect), offset(offset), duration(duration) {}
 
-// begin RenderStates
-RenderStates::RenderStates() : state("idle"), offset(0) {}
-
-void RenderStates::pushFrame(std::string state_arg, RenderFrame renderframe) {
-  states.at(state_arg).push_back(renderframe);
+// begin RenderState
+RenderState::RenderState() : RenderState("idle", 0) {
 }
 
-void RenderStates::pushFrame(std::string state_arg, std::string texture,
-                             Rect<int> cliprect, Vec2f offset_arg, float duration) {
-  states[state_arg].push_back(
-    RenderFrame{texture, cliprect, offset_arg, duration}
+RenderState::RenderState(std::string state, std::size_t offset)
+: state(state), offset(offset) {}
+
+void RenderState::setState(std::string state_new, std::size_t offset_new) {
+  state = state_new;
+  offset = offset_new;
+}
+
+void RenderState::setState(std::string state_new) {
+  setState(state_new, 0);
+}
+
+const std::string& RenderState::getState() const {
+  return state;
+}
+
+std::size_t RenderState::getOffset() const {
+  return offset;
+}
+// end RenderState
+
+// begin RenderStates
+RenderStates::RenderStates() {}
+
+void RenderStates::pushFrame(std::string state, RenderFrame renderframe) {
+  states.at(state).push_back(renderframe);
+}
+
+void RenderStates::pushFrame(std::string state, std::string texture,
+                             Rect<int> cliprect, Vec2f offset, float duration) {
+  states[state].push_back(
+    RenderFrame{texture, cliprect, offset, duration}
   );
 }
 
-void RenderStates::pushFrame(std::string state_arg, std::string texture,
-                             Vec2i origin, Vec2f offset_arg, float duration) {
-  pushFrame(state_arg, texture, Rect<int>(origin, Vec2i(16, 16)), offset_arg, duration);
+void RenderStates::pushFrame(std::string state, std::string texture,
+                             Vec2i origin, Vec2f offset, float duration) {
+  pushFrame(state, texture, Rect<int>(origin, Vec2i(16, 16)), offset, duration);
 }
 
 StringList RenderStates::getStateList() const {
@@ -37,11 +62,11 @@ StringList RenderStates::getStateList() const {
   return state_list;
 }
 
-std::size_t RenderStates::getFrameCount(std::string state_arg) const {
-  return states.at(state_arg).size();
+std::size_t RenderStates::getFrameCount(std::string state) const {
+  return states.at(state).size();
 }
 
-std::size_t RenderStates::getFrameOffset(float time) const {
+std::size_t RenderStates::getFrameOffset(std::string state, float time) const {
   const RenderFrames& frames = states.at(state);
   float time_max = 0.f;
 
@@ -60,20 +85,11 @@ std::size_t RenderStates::getFrameOffset(float time) const {
   return (counter - 1) % frames.size();
 }
 
-void RenderStates::setState(std::string state_arg, std::size_t offset_arg) {
-  state = state_arg;
-  offset = offset_arg;
+const RenderFrame& RenderStates::getFrame(const RenderState& state) const {
+  return getFrame(state.getState(), state.getOffset());
 }
 
-void RenderStates::setState(std::string state_arg) {
-  setState(state_arg, 0);
-}
-
-const std::string& RenderStates::getState() const {
-  return state;
-}
-
-const RenderFrame& RenderStates::getFrame() const {
+const RenderFrame& RenderStates::getFrame(std::string state, std::size_t offset) const {
   return states.at(state).at(offset);
 }
 // end RenderStates
--- src/renderstates.hpp
+++ src/renderstates.hpp
@@ -19,6 +19,22 @@ struct RenderFrame {
   RenderFrame(std::string texture, Rect<int> cliprect, Vec2f offset, float duration);
 };
 
+class RenderState {
+public:
+  RenderState();
+  RenderState(std::string state, std::size_t offset);
+
+  void setState(std::string state);
+  void setState(std::string state, std::size_t offset);
+
+  const std::string& getState() const;
+  std::size_t getOffset() const;
+
+private:
+  std::string state;
+  std::size_t offset;
+};
+
 class RenderStates {
 public:
   RenderStates();
@@ -27,20 +43,15 @@ public:
   void pushFrame(std::string, std::string, Vec2i, Vec2f, float);
   void pushFrame(std::string, std::string, Rect<int>, Vec2f, float);
 
-  void setState(std::string);
-  void setState(std::string, std::size_t);
-
   StringList getStateList() const;
 
   std::size_t getFrameCount(std::string state_arg) const;
-  std::size_t getFrameOffset(float time) const;
+  std::size_t getFrameOffset(std::string state, float time) const;
 
-  const std::string& getState() const;
-  const RenderFrame& getFrame() const;
+  const RenderFrame& getFrame(const RenderState& state) const;
+  const RenderFrame& getFrame(std::string state, std::size_t offset) const;
 
 private:
-  std::string state;
-  std::size_t offset;
   std::map<std::string, RenderFrames> states;
 };
 }
