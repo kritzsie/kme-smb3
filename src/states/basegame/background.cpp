353ecd83cfa0b4d28e9906703f33213aa839c1f9
Random "Kritzsie" Hobo <kritzsie@gmail.com>
2022-05-03 05:12:58 +0100

    Bit more smart pointer cleanup

--- src/states/basegame/background.cpp
+++ src/states/basegame/background.cpp
@@ -2,15 +2,14 @@
 
 #include "../../renderstates.hpp"
 
-#include <memory>
 #include <string>
 
 namespace kme {
 void Background::registerBackground(std::string name, RenderFrames background) {
-  backgrounds.emplace(name, std::make_shared<RenderFrames>(std::move(background)));
+  backgrounds.emplace(name, std::move(background));
 }
 
-std::shared_ptr<const RenderFrames> Background::getBackground(std::string name) const {
+const RenderFrames& Background::getBackground(std::string name) const {
   return backgrounds.at(name);
 }
 }
--- src/states/basegame/background.hpp
+++ src/states/basegame/background.hpp
@@ -3,16 +3,15 @@
 #include "../../renderstates.hpp"
 #include "../../types.hpp"
 
-#include <memory>
 #include <string>
 
 namespace kme {
 struct Background {
 public:
   void registerBackground(std::string name, RenderFrames background);
-  std::shared_ptr<const RenderFrames> getBackground(std::string name) const;
+  const RenderFrames& getBackground(std::string name) const;
 
 private:
-  StringTable<std::shared_ptr<RenderFrames>> backgrounds;
+  StringTable<RenderFrames> backgrounds;
 };
 }
