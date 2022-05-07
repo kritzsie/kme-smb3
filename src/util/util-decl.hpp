cbf244f6aa787a133e29dbccc39c376440eb1c4f
Random "Kritzsie" Hobo <kritzsie@gmail.com>
2022-05-07 08:03:45 +0100

    Sign multiplication is now type-commutative

--- src/util/util-decl.hpp
+++ src/util/util-decl.hpp
@@ -2,6 +2,8 @@
 
 #include "../types.hpp"
 
+#include <type_traits>
+
 namespace kme {
 // Strongly-typed sign scalar
 enum class Sign { PLUS, MINUS };
@@ -9,11 +11,14 @@ enum class Sign { PLUS, MINUS };
 Sign operator +(const Sign& rhs);
 Sign operator -(const Sign& rhs);
 
-template<typename T>
-T operator *(const Sign& lhs, const T& rhs);
+template<
+  typename T,
+  std::enable_if_t<not std::is_same<Sign, T>::value, std::nullptr_t> = nullptr
+>
+T operator *(const T& lhs, const Sign& rhs);
 
 template<typename T>
-T operator *(const T& lhs, const Sign& rhs);
+T operator *(const Sign& lhs, const T& rhs);
 
 template<typename T>
 Sign toSign(const T& value);
@@ -22,10 +27,12 @@ Sign toSign(const T& value);
 // These can be used to perform range calculations such that, if x and y are of
 // some type T, then typename Widen<T>::type is guaranteed to be wide enough to
 // contain the result of x - y. For example:
-// Byte x = -10;
-// Byte y = 120;
-// Byte narrow = x - y; // Byte is not large enough to represent -130
-// typename Widen<Byte>::type wide = x - y; // But this type is
+// ```c++
+// using WideByte = Widen<Byte>::type;
+// Byte x1 = 100, x2 = 50;
+// Byte     dx   = x2 - x1; // Byte is not wide enough to represent -50
+// WideByte wide = x2 - x1; // But this type is
+// ```
 template<typename T>
 struct Widen {};
 
@@ -39,6 +46,7 @@ template<> struct Widen<UInt8>  { using type = SInt16; };
 template<> struct Widen<UInt16> { using type = SInt32; };
 template<> struct Widen<UInt32> { using type = SInt64; };
 
+// floating-point types are a mild exception
 template<> struct Widen<float>       { using type = float; };
 template<> struct Widen<double>      { using type = double; };
 template<> struct Widen<long double> { using type = long double; };
--- src/util/util-impl.hpp
+++ src/util/util-impl.hpp
@@ -1,16 +1,21 @@
 #pragma once
 
+#include <type_traits>
+
 #include <cmath>
 
 namespace kme {
-template<typename T>
-T operator *(const Sign& lhs, const T& rhs) {
-  return lhs == Sign::PLUS ? rhs : -rhs;
+template<
+  typename T,
+  std::enable_if_t<not std::is_same<Sign, T>::value, std::nullptr_t>
+>
+T operator *(const T& lhs, const Sign& rhs) {
+  return rhs * lhs;
 }
 
 template<typename T>
-T operator *(const T& lhs, const Sign& rhs) {
-  return rhs * lhs;
+T operator *(const Sign& lhs, const T& rhs) {
+  return lhs == Sign::PLUS ? rhs : -rhs;
 }
 
 template<typename T>
