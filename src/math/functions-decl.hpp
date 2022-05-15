6b4dd9177d52e170692784e09f6deaba41c40bce
Random "Kritzsie" Hobo <kritzsie@gmail.com>
2022-05-15 04:24:42 +0100

    map function can now take const reference as argument

--- src/math/functions-decl.hpp
+++ src/math/functions-decl.hpp
@@ -3,4 +3,7 @@
 namespace kme::fp {
 template<typename T, typename U>
 constexpr U map(T (&f)(T), const U& self);
+
+template<typename T, typename U>
+constexpr U map(T (&f)(const T&), const U& self);
 }
--- src/math/functions-impl.hpp
+++ src/math/functions-impl.hpp
@@ -12,7 +12,17 @@ constexpr Vec2<T> map(T (&f)(T), const Vec2<T>& vec2) {
 }
 
 template<typename T>
+constexpr Vec2<T> map(T (&f)(const T&), const Vec2<T>& vec2) {
+  return Vec2<T>(f(vec2.x), f(vec2.y));
+}
+
+template<typename T>
 constexpr Rect<T> map(T (&f)(T), const Rect<T>& rect) {
+  return Rect<T>(map(f, rect.pos), map(f, rect.size));
+}
+
+template<typename T>
+constexpr Rect<T> map(T (&f)(const T&), const Rect<T>& rect) {
   return Rect<T>(map(f, rect.pos), map(f, rect.size));
 }
 }
